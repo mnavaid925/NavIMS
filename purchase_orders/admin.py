d5341d63be89bf4af967db067371f77bee3256ac
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-05 01:49:15 +0500

    Register PurchaseOrderDispatch in admin with inline

--- purchase_orders/admin.py
+++ purchase_orders/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import PurchaseOrder, PurchaseOrderItem, ApprovalRule, PurchaseOrderApproval
+from .models import (
+    PurchaseOrder, PurchaseOrderItem, ApprovalRule,
+    PurchaseOrderApproval, PurchaseOrderDispatch,
+)
 
 
 class PurchaseOrderItemInline(admin.TabularInline):
@@ -12,12 +15,17 @@ class PurchaseOrderApprovalInline(admin.TabularInline):
     extra = 0
 
 
+class PurchaseOrderDispatchInline(admin.TabularInline):
+    model = PurchaseOrderDispatch
+    extra = 0
+
+
 @admin.register(PurchaseOrder)
 class PurchaseOrderAdmin(admin.ModelAdmin):
     list_display = ('po_number', 'tenant', 'vendor', 'status', 'order_date', 'payment_terms', 'created_by', 'created_at')
     list_filter = ('status', 'payment_terms', 'tenant')
     search_fields = ('po_number', 'vendor__company_name')
-    inlines = [PurchaseOrderItemInline, PurchaseOrderApprovalInline]
+    inlines = [PurchaseOrderItemInline, PurchaseOrderApprovalInline, PurchaseOrderDispatchInline]
 
 
 @admin.register(PurchaseOrderItem)
@@ -39,3 +47,10 @@ class PurchaseOrderApprovalAdmin(admin.ModelAdmin):
     list_display = ('purchase_order', 'tenant', 'approver', 'decision', 'decided_at')
     list_filter = ('decision', 'tenant')
     search_fields = ('purchase_order__po_number', 'approver__username')
+
+
+@admin.register(PurchaseOrderDispatch)
+class PurchaseOrderDispatchAdmin(admin.ModelAdmin):
+    list_display = ('purchase_order', 'tenant', 'dispatch_method', 'sent_to_email', 'sent_by', 'dispatched_at')
+    list_filter = ('dispatch_method', 'tenant')
+    search_fields = ('purchase_order__po_number', 'sent_to_email')
