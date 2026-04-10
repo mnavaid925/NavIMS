712f2d2f8f97672b43f2b2b28e20d5dbf2a58fdf
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-10 23:52:17 +0500

    Add stock_movements URL include

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('receiving/', include('receiving.urls')),
     path('warehousing/', include('warehousing.urls')),
     path('inventory/', include('inventory.urls')),
+    path('stock-movements/', include('stock_movements.urls')),
 ]
 
 if settings.DEBUG:
