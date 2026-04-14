c5bb3835002d9395a0903a9bce6ca3722ba1f908
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-14 21:51:13 +0500

    Add StocktakingConfig app config

---
+++ stocktaking/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class StocktakingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stocktaking'
+    verbose_name = 'Stocktaking & Cycle Counting'
