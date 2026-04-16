741f9d46ac031be8047e8de31fe2a9b78a052acc
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-16 23:56:21 +0500

    Register MultiLocationConfig for multi_location app

---
+++ multi_location/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MultiLocationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'multi_location'
+    verbose_name = 'Multi-Location Management'
