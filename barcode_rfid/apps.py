3fb76cb3250505f622e65259bfb92cdbfd27a770
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-19 01:22:28 +0500

    feat(barcode_rfid): AppConfig

---
+++ barcode_rfid/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BarcodeRfidConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'barcode_rfid'
+    verbose_name = 'Barcode & RFID Integration'
