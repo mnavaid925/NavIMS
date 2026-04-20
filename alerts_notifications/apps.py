a85088bf6eba546371846dc558af811a3ccfce1b
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-20 23:31:24 +0500

    feat(M17): AppConfig with verbose_name Alerts & Notifications

---
+++ alerts_notifications/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AlertsNotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'alerts_notifications'
+    verbose_name = 'Alerts & Notifications'
