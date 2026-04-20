77a9e1603ac88f4fad65a150e98bab6cc34ee01b
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-04-20 22:13:56 +0500

    quality_control(M16): AppConfig

---
+++ quality_control/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class QualityControlConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'quality_control'
+    verbose_name = 'Quality Control & Inspection'
